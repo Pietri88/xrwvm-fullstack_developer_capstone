223b2c4053793bf9fbfe4c1ff41b3554527e2c2b
Pietri88 <116376883+Pietri88@users.noreply.github.com>
2025-01-21 20:34:24 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -39,31 +39,32 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-     data = json.loads(request.body)
-     username = data['userName']
-     password = data['password']
-     first_name = data['firstName']
-     last_name = data['lastName']
-     email = data['email']
-     username_exist = False
-     try:
-         User.objects.get(username=username)
-         username_exist = True
-     except Exception as err: # Correcting the exception variable
-         # If not, log this is a new user
-         logger.debug("{} is new user".format(username))
-     if not username_exist:
-         user = User.objects.create_user(username=username,
-                                         first_name=first_name,
-                                         last_name=last_name,
-                                         password=password,
-                                         email=email)
-         login(request, user)
-         data = {"userName": username, "status": "Authenticated"}
-         return JsonResponse(data)
-     else:
-         data = {"userName": username, "error": "Already Registered"}
-         return JsonResponse(data)
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    try:
+        User.objects.get(username=username)
+        username_exist = True
+    except Exception as err: # Correcting the exception variable
+        # If not, log this is a new user
+        logger.debug("{} is new user".format(username))
+    if not username_exist:
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
+        login(request, user)
+        data = {"userName": username, "status": "Authenticated"}
+        return JsonResponse(data)
+    else:
+        data = {"userName": username, "error": "Already Registered"}
+        return JsonResponse(data)
+
 
 
 def get_cars(request):
